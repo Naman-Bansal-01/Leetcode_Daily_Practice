535d5ce1f04b455f215425aefe730b1850004a7f
Naman Bansal <118118762+Naman-Bansal-01@users.noreply.github.com>
2025-08-04 00:42:58 +0530

    Time: 4 ms (57.24%), Space: 15 MB (13.8%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -1,6 +1,18 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        
+        map<int,int>mpp;
+        vector<int>res;
+        for(int i=0;i<nums.size();i++){
+            int req=target-nums[i];
+            if(mpp.find(req) != mpp.end())
+            {
+                res.push_back(i);
+                res.push_back(mpp[req]);
+                return res;
+            }
+             mpp[nums[i]]=i;
+        }
+      return res; 
     }
 };
