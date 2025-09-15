14ef9491782ccef9934bb04dd0ed11c6b1696c01
Naman Bansal <118118762+Naman-Bansal-01@users.noreply.github.com>
2025-09-16 03:01:10 +0530

    Time: 0 ms (100%), Space: 23.9 MB (20.95%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.cpp
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -1,13 +1,16 @@
 class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
-        int a;
+        int a = INT_MAX;
         int n = nums.size();
         for(int i=0;i<n;i++){
             if(nums[i]==0){
                 a=i;
                 break;
             }
+        }
+        if(a >=n){
+            return;
         }
         // while(nums[a]!=0 && a<n){
         //     a++;
