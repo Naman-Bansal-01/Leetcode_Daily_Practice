363f10380af8004e3dc984be233efbf561632370
Naman Bansal <118118762+Naman-Bansal-01@users.noreply.github.com>
2025-09-23 03:05:13 +0530

    Time: 0 ms (100%), Space: 20.7 MB (58.09%) - LeetHub

--- 0136-single-number/0136-single-number.cpp
+++ 0136-single-number/0136-single-number.cpp
@@ -1,18 +1,10 @@
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
-        unordered_map<int,int> mp;
-        for(int i=0; i<nums.size(); i++){
-            mp[nums[i]]+=1;
-
+        int xorr = 0;
+        for (int i = 0; i < nums.size(); i++) {
+            xorr = xorr ^ nums[i];
         }
-        int val;
-        for(auto &p: mp){
-            if(p.second==1){
-                val=p.first;
-                break;
-            }
-        }
-       return val; 
+        return xorr;
     }
 };
