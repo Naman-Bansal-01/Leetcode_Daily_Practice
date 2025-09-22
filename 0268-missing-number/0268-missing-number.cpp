0aeb765a69761ece356c4f70031d6a29519566dd
Naman Bansal <118118762+Naman-Bansal-01@users.noreply.github.com>
2025-09-23 03:47:37 +0530

    Time: 0 ms (100%), Space: 21.6 MB (99.66%) - LeetHub

--- 0268-missing-number/0268-missing-number.cpp
+++ 0268-missing-number/0268-missing-number.cpp
@@ -3,11 +3,12 @@ public:
     int missingNumber(vector<int>& nums) {
         int n=nums.size();
         int a=n*(n+1)/2;
+        int b=0;
         for(int x:nums){
-            a-=x;
+            b+=x;
 
         }
-        return a;
+        return a-b;
         
     }
 };
